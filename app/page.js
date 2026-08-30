c5022e79314619491efe940538a8eb57c86e19ed
hamzabougurra-svg <hamzabougurra@gmail.com>
2026-08-30 18:10:59 +0100

    Add service worker registration in Home component
    Register service worker for offline capabilities.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js");
+    }
+  }, []);
+
   return (
     <main dir="rtl" style={mainStyle}>
       <div style={containerStyle}>
